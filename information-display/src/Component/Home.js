5b0aa5e5ae61c538aaffd5367863ed91a9d24212
Owen Ekhator <owenekhator@rocketmail.com>
2019-02-23 23:44:31 -0600

    added home component

---
+++ information-display/src/Component/Home.js
@@ -0,0 +1,26 @@
+import React,  { Component } from 'React'
+import { ScrollView, ActivityIndicator, StyleSheet } from 'react-native'
+class Home extends Component {
+
+  state = {
+    users: [],
+    loading: true
+  }
+
+  componentDidMount()
+
+ render(){
+
+    return (
+
+
+
+
+    )
+
+ }
+
+}
+
+
+export default Home
